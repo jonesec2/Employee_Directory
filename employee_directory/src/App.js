2eef0f40d64c57f67ab84a7c504e55e6140fa912
pb-totoro <jonesec2@gmail.com>
2020-03-24 14:21:59 -0400

    Created a table for members using bootstrap

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -11,16 +11,33 @@ class App extends Component {
 
    render() {
       return (
-         this.state.employees.map(employee => (
-            <EmployeeComponent
-               id={employee.id}
-               firstName={employee.firstName}
-               lastName={employee.lastName}
-               position={employee.position}
-               contractor={employee.contractor}
-               management={employee.management}
-            />
-         ))
+         <>
+            <table className="table table-striped">
+               <thead>
+                  <tr>
+                     <th id="id" scope="col">#</th>
+                     <th scope="col">First</th>
+                     <th scope="col">Last</th>
+                     <th scope="col">Position</th>
+                     <th scope="col">Contractor</th>
+                     <th scope="col">Management</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  {this.state.employees.map(employee => (
+                     <EmployeeComponent
+
+                        id={employee.id}
+                        firstName={employee.firstName}
+                        lastName={employee.lastName}
+                        position={employee.position}
+                        contractor={employee.contractor}
+                        management={employee.management}
+                     />
+                  ))}
+               </tbody>
+            </table>
+         </>
       )
    }
 }
--- employee_directory/src/components/EmployeeComponent.js
+++ employee_directory/src/components/EmployeeComponent.js
@@ -1,18 +1,31 @@
 import React from "react";
 
+// function EmployeeComponent(props) {
+//    return (
+//       <div className="card">
+//          <h1>Employee: {props.id}</h1>
+//          <h3>Name: {props.firstName} {props.lastName} </h3>
+//          <h5>Stats</h5>
+//          <ul>
+//             <li>Position: {props.position}</li>
+//             <li>Contractor: {props.contractor}</li>
+//             <li>Management: {props.management}</li>
+//          </ul>
+//       </div>
+//    );
+// }
+
 function EmployeeComponent(props) {
-  return (
-    <div className="card">
-      <h1>Employee: {props.id}</h1>
-      <h3>Name: {props.firstName} {props.lastName} </h3>
-      <h5>Stats</h5>
-      <ul>
-        <li>Position: {props.position}</li>
-        <li>Contractor: {props.contractor}</li>
-        <li>Management: {props.management}</li>
-      </ul>
-    </div>
-  );
+   return (
+      <tr>
+         <th scope="row">{props.id}</th>
+         <td>{props.firstName}</td>
+         <td>{props.lastName}</td>
+         <td>{props.position}</td>
+         <td>{props.contractor}</td>
+         <td>{props.management}</td>
+      </tr>
+   );
 }
 
 export default EmployeeComponent;
