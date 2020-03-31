4a8720cfcf9f5211717c5ce640123d434e1ab554
Christian Jones <jonesec2@gmail.com>
2020-03-31 15:13:10 -0400

    Update InputForms.js

--- src/pages/InputForms.js
+++ src/pages/InputForms.js
@@ -5,7 +5,7 @@ function InputForms(props) {
    return (
       <form onSubmit={props.handleSubmit} className="mx-auto">
          <div className="form-group row">
-            <label className="col-sm-6 col-form-label" htmlFor="exampleFormControlSelect1">Management?</label>
+            <label className="col-sm-6 col-form-label" htmlFor="exampleFormControlSelect1">Filter: </label>
             <div className="col-sm-6">
                <select value={props.manageValue} className="form-control" onChange={props.handleChange} id="exampleFormControlSelect1" placeholder="-- Select --">
                   <option key={"Select"} manageValue=" Select " selected disabled>Select</option>
